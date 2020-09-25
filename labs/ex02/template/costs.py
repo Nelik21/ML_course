cf9b8e6ee9903e0c7d731e6865edd46e2aba2307
Nelik21 <71333948+Nelik21@users.noreply.github.com>
2020-09-25 19:41:04 +0200

    Update costs.py

--- labs/ex02/template/costs.py
+++ labs/ex02/template/costs.py
@@ -2,12 +2,5 @@
 """Function used to compute the loss."""
 
 def compute_loss(y, tx, w):
-    """Calculate the loss.
-
-    You can calculate the loss using mse or mae.
-    """
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # TODO: compute loss by MSE / MAE
-    # ***************************************************
+    return (y-tx*w).transpose()@(y-tx*w)
     raise NotImplementedError
